8bedc376d3ae0d00d7fddf6aa0930d57ebd978c2
nickmoreton <nickmoreton@me.com>
2024-10-06 21:25:08 +0100

    Add tags to the blog pages

--- app/blog/models.py
+++ app/blog/models.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.db import models
+from modelcluster.contrib.taggit import ClusterTaggableManager
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from taggit.models import TaggedItemBase
 from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.fields import RichTextField
 from wagtail.models import Orderable, Page
@@ -21,11 +23,18 @@ class BlogIndexPage(Page):
         return context
 
 
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        "BlogPage", related_name="tagged_items", on_delete=models.CASCADE
+    )
+
+
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
     authors = ParentalManyToManyField("blog.Author", blank=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
 
     search_fields = Page.search_fields + [
         index.SearchField("intro"),
@@ -37,6 +46,7 @@ class BlogPage(Page):
             [
                 FieldPanel("date"),
                 FieldPanel("authors", widget=forms.CheckboxSelectMultiple),
+                FieldPanel("tags"),
             ],
             heading="Blog information",
         ),
