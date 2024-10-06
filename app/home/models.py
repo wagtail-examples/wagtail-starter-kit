c19df78296a50b6f667af0768e648542038bee93
nickmoreton <nickmoreton@me.com>
2024-10-06 13:20:11 +0100

    Extend the home page model

--- app/home/models.py
+++ app/home/models.py
@@ -1,9 +1,11 @@
-from wagtail import __version__ as WAGTAIL_VERSION
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField
 from wagtail.models import Page
 
 
 class HomePage(Page):
-    def get_context(self, request):
-        context = super().get_context(request)
-        context["wagtail_version"] = WAGTAIL_VERSION
-        return context
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
